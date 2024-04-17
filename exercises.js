258134b043fa89b57da0827043f44b62024e783c
ShenalGanegoda <106686816+ShenalGanegoda@users.noreply.github.com>
2024-04-17 08:20:16 +0530

    React app start for Javascript-exercises

--- exercises.js
+++
@@ -1,8 +0,0 @@
-// Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
-leapYearCheck(2028);
-
-function leapYearCheck(year) {
-  if (year % 4 == 0) {
-    console.log("It's a leap year!");
-  } else console.log("Not a leap year.");
-}
