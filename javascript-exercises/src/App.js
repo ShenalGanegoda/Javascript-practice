07ebc378699b3ba135f39ff5f55e149e128a2fc9
ShenalGanegoda <106686816+ShenalGanegoda@users.noreply.github.com>
2024-04-20 11:35:48 +0530

    Sum of integers simple exercise completed

--- javascript-exercises/src/App.js
+++ javascript-exercises/src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
-function App() {}
+function sumOfintegers(num01, num02) {
+  /*
+Write a JavaScript program to compute the sum of the two given integers.
+If the two values are the same, then return triple their sum. 
+*/
+
+  if (num01 != num02) {
+    return num01 + num02;
+  } else return (num01 + num02) * 3;
+}
+
+function App() {
+  return <h1>{sumOfintegers(12, 12)}</h1>;
+}
 
 export default App;
